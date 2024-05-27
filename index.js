bc8567ebac2d33f9d54d2d23e9c2ff197da57ae1
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-05-27 18:41:26 +0300

    WebStudio

---
+++ index.js
@@ -0,0 +1,17 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+      modal1: document.querySelector("[data-modal1]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.modal1.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
